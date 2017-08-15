3f51e00ab54a0c0a3b129d3b2c76271f8b9aa023
Daniel Gilbert <Daniel.Stephen.Gilbert@gmail.com>
2017-08-15 12:49:23 -0700

    Add avl_tree_unittest:TEST(insertNodeWithKeyTest, RightRightHeavyCase1) Add avl_tree_unittest:TEST(insertNodeWithKeyTest, RightRightHeavyCase2) Add avl_tree_unittest:TEST(insertNodeWithKeyTest, RightLeftHeavyCase1) Add avl_tree_unittest:TEST(insertNodeWithKeyTest, RightLeftHeavyCase2)
    Signed-off-by: Daniel Gilbert <Daniel.Stephen.Gilbert@gmail.com>

--- test/avl_tree_unittest.cpp
+++ test/avl_tree_unittest.cpp
@@ -131,3 +131,133 @@ TEST(insertNodeWithKeyTest, LeftRightHeavyCase2) {
     EXPECT_EQ(node1->getHeight(), 1);
     EXPECT_EQ(node0->getHeight(), 0);
 }
+
+TEST(insertNodeWithKeyTest, RightRightHeavyCase1) {
+    avl_tree<int> avlt;
+    avlt.insertNodeWithKey(0);
+    avlt.insertNodeWithKey(1);
+    avlt.insertNodeWithKey(2);
+    stringstream ss;
+    avlt.printTree(ss);
+    string expectedOutput = 
+        string("\t2\n") +
+        string("1\n") +
+        string("\t0\n");
+    EXPECT_EQ(ss.str(), expectedOutput);
+    auto node2 = avlt.getNodeWithKey(2);
+    auto node1 = avlt.getNodeWithKey(1);
+    auto node0 = avlt.getNodeWithKey(0);
+    // test parents
+    EXPECT_EQ(node2->getParent(), node1);
+    EXPECT_EQ(node1->getParent(), nullptr);
+    EXPECT_EQ(node0->getParent(), node1);
+    // test heights
+    EXPECT_EQ(node2->getHeight(), 0);
+    EXPECT_EQ(node1->getHeight(), 1);
+    EXPECT_EQ(node0->getHeight(), 0);
+}
+
+TEST(insertNodeWithKeyTest, RightRightHeavyCase2) {
+    avl_tree<int> avlt;
+    avlt.insertNodeWithKey(1);
+    avlt.insertNodeWithKey(0);
+    avlt.insertNodeWithKey(3);
+    avlt.insertNodeWithKey(2);
+    avlt.insertNodeWithKey(4);
+    avlt.insertNodeWithKey(5);
+    stringstream ss;
+    avlt.printTree(ss);
+    string expectedOutput = 
+        string("\t\t5\n") +
+        string("\t4\n") +
+        string("3\n") +
+        string("\t\t2\n") +
+        string("\t1\n") +
+        string("\t\t0\n");
+    EXPECT_EQ(ss.str(), expectedOutput);
+    auto node5 = avlt.getNodeWithKey(5);
+    auto node4 = avlt.getNodeWithKey(4);
+    auto node3 = avlt.getNodeWithKey(3);
+    auto node2 = avlt.getNodeWithKey(2);
+    auto node1 = avlt.getNodeWithKey(1);
+    auto node0 = avlt.getNodeWithKey(0);
+    // test parents
+    EXPECT_EQ(node5->getParent(), node4);
+    EXPECT_EQ(node4->getParent(), node3);
+    EXPECT_EQ(node3->getParent(), nullptr);
+    EXPECT_EQ(node2->getParent(), node1);
+    EXPECT_EQ(node1->getParent(), node3);
+    EXPECT_EQ(node0->getParent(), node1);
+    // test heights
+    EXPECT_EQ(node5->getHeight(), 0);
+    EXPECT_EQ(node4->getHeight(), 1);
+    EXPECT_EQ(node3->getHeight(), 2);
+    EXPECT_EQ(node2->getHeight(), 0);
+    EXPECT_EQ(node1->getHeight(), 1);
+    EXPECT_EQ(node0->getHeight(), 0);
+}
+
+TEST(insertNodeWithKeyTest, RightLeftHeavyCase1) {
+    avl_tree<int> avlt;
+    avlt.insertNodeWithKey(0);
+    avlt.insertNodeWithKey(2);
+    avlt.insertNodeWithKey(1);
+    stringstream ss;
+    avlt.printTree(ss);
+    string expectedOutput = 
+        string("\t2\n") +
+        string("1\n") +
+        string("\t0\n");
+    EXPECT_EQ(ss.str(), expectedOutput);
+    auto node2 = avlt.getNodeWithKey(2);
+    auto node1 = avlt.getNodeWithKey(1);
+    auto node0 = avlt.getNodeWithKey(0);
+    // test parents
+    EXPECT_EQ(node2->getParent(), node1);
+    EXPECT_EQ(node1->getParent(), nullptr);
+    EXPECT_EQ(node0->getParent(), node1);
+    // test heights
+    EXPECT_EQ(node2->getHeight(), 0);
+    EXPECT_EQ(node1->getHeight(), 1);
+    EXPECT_EQ(node0->getHeight(), 0);
+}
+
+TEST(insertNodeWithKeyTest, RightLeftHeavyCase2) {
+    avl_tree<int> avlt;
+    avlt.insertNodeWithKey(1);
+    avlt.insertNodeWithKey(0);
+    avlt.insertNodeWithKey(4);
+    avlt.insertNodeWithKey(3);
+    avlt.insertNodeWithKey(5);
+    avlt.insertNodeWithKey(2);
+    stringstream ss;
+    avlt.printTree(ss);
+    string expectedOutput = 
+        string("\t\t5\n") +
+        string("\t4\n") +
+        string("3\n") +
+        string("\t\t2\n") +
+        string("\t1\n") +
+        string("\t\t0\n");
+    EXPECT_EQ(ss.str(), expectedOutput);
+    auto node5 = avlt.getNodeWithKey(5);
+    auto node4 = avlt.getNodeWithKey(4);
+    auto node3 = avlt.getNodeWithKey(3);
+    auto node2 = avlt.getNodeWithKey(2);
+    auto node1 = avlt.getNodeWithKey(1);
+    auto node0 = avlt.getNodeWithKey(0);
+    // test parents
+    EXPECT_EQ(node5->getParent(), node4);
+    EXPECT_EQ(node4->getParent(), node3);
+    EXPECT_EQ(node3->getParent(), nullptr);
+    EXPECT_EQ(node2->getParent(), node1);
+    EXPECT_EQ(node1->getParent(), node3);
+    EXPECT_EQ(node0->getParent(), node1);
+    // test heights
+    EXPECT_EQ(node5->getHeight(), 0);
+    EXPECT_EQ(node4->getHeight(), 1);
+    EXPECT_EQ(node3->getHeight(), 2);
+    EXPECT_EQ(node2->getHeight(), 0);
+    EXPECT_EQ(node1->getHeight(), 1);
+    EXPECT_EQ(node0->getHeight(), 0);
+}
